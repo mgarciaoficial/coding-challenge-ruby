33a7680c881e850a4cb28172c99b3e66cc8e846a
Licenciado <mgarcia@codigodelsur.com>
2020-11-05 02:39:53 -0300

    Add tenant key verification.

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
 class Api::ApplicationController < ActionController::API
-  before_action :track_request
+  before_action :authenticate_tenant, :track_request
+
+  rescue_from UnauthError, with: :render_unauth
 
   protected
+
+  def authenticate_tenant
+    api_key = request.headers['X-API-KEY']
+    raise UnauthError, 'Invalid key' unless Tenant.exists?(api_key: api_key)
+  end
 
   def track_request
     # track request logic goes in here
   end
+
+  private
+
+  def render_unauth(exception)
+    render json: { message: exception.message }, status: :unauthorized
+  end
+
 end
--- spec/requests/api/questions/index_spec.rb
+++ spec/requests/api/questions/index_spec.rb
@@ -2,19 +2,40 @@ require 'rails_helper'
 
 describe 'GET api/questions/', type: :request do
   before do
-    get api_questions_path, as: :json
+    get api_questions_path, headers: headers, as: :json
+  end
+  let(:headers) { { 'X-API-KEY' => api_key } }
+
+  context 'when no api_key is provided' do
+    let(:api_key) { nil }
+
+    it 'returns 401 Unauthorized status' do
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 
-  it 'returns 200 OK status' do
-    expect(response).to have_http_status(:ok)
+  context 'when an invalid key is provided' do
+    let(:api_key) { 'madeInRohan'}
+
+    it 'returns 401 Unauthorized status' do
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 
-  it 'complies to json:api conventions' do
-    expect(json).to match_response_schema("questions")
-  end
+  context 'when an valid key is provided' do
+    let(:api_key) { Tenant.pluck(:api_key).sample }
 
-  # Author's note: specs are run against seed data
-  it 'fetches shared questions only' do
-    expect(json.dig(:data, :questions).length).to eq(24)
+    it 'returns 200 OK status' do
+      expect(response).to have_http_status(:ok)
+    end
+  
+    it 'complies to json:api conventions' do
+      expect(json).to match_response_schema("questions")
+    end
+  
+    # Author's note: specs are run against seed data
+    it 'fetches shared questions only' do
+      expect(json.dig(:data, :questions).length).to eq(24)
+    end
   end
 end
