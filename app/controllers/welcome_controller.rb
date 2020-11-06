cd40d11e819ef32561100068d8728e0350d79862
Licenciado <mgarcia@codigodelsur.com>
2020-11-06 02:10:02 -0300

    Add site info dashboard.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @user_count = User.count
+    @question_count = Question.count
+    @answer_count = Answer.count
+    @tenants = Tenant.select(:name, :req_count)
   end
 end
